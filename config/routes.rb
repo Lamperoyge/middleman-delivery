673d5b7a6555291df49e22b1b3c643a5d6331f55
Adrian Mustea <mustea.adrian95@gmail.com>
2017-11-01 18:14:46 +0200

    routes test 1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :ratings, only: [:create]
+    resources :dishes
+  end
+    resources :orders do
+      resources :dishes
+  end
+
 end
